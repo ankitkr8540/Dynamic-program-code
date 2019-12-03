4a453aeddef62995e0de2c41b0a49d0598b7ff76
ankitkr8540 <33374891+ankitkr8540@users.noreply.github.com>
2019-12-03 21:37:50 +0530

    Update mpi5.c

--- mpi5.c
+++ mpi5.c
@@ -6,7 +6,7 @@ double func( double x)
 return (4.0 / (1.0 + x*x));
 }
 int
-main( int argc, char *arg[])
+main( int argc, char *argv[])
 {
 int NoInterval, interval;
 int MyRank, Numprocs, Root = 0;
@@ -32,7 +32,7 @@ h = 1.0 / ( double )NoInterval;
 sum = 0.0;
 for (interval = MyRank + 1; interval <= NoInterval; interval += Numprocs)
 {
-x = h * (( double )interval 0.5);
+x = h * (( double )interval-0.5);
 sum += func(x);
 }
 mypi = h * sum;
@@ -40,7 +40,7 @@ mypi = h * sum;
 MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, Root, MPI_COMM_WORLD);
 if (MyRank == Root)
 {
-printf("pi is approximately %.16f, Error is %.16f \n",pi, fabs(pi PI25DT));
+printf("pi is approximately %.16f, Error is %.16f \n",pi, fabs(pi-PI25DT));
 }
 MPI_Finalize();
 }
