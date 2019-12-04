66b8b25f92d30ac73d5b7642f1e037b116468a63
ankitkr8540 <33374891+ankitkr8540@users.noreply.github.com>
2019-12-04 09:55:19 +0530

    Update mpi5.c

--- mpi5.c
+++ mpi5.c
@@ -1,46 +1,17 @@
-#include<stdio.h>
-#include<math.h>
-#include<mpi.h>
-double func( double x)
+#include <stdio.h>
+#include<string.h>
+#include <mpi.h>
+#define BUFFER_SIZE 32
+int main (int argc, char *argv[])
 {
-return (4.0 / (1.0 + x*x));
-}
-int
-main( int argc, char *argv[])
-{
-int NoInterval, interval;
-int MyRank, Numprocs, Root = 0;
-double mypi, pi, h, sum, x;
-double PI25DT = 3.141592653589793238462643;
-
-MPI_Init(&argc,&argv);
-MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
-MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
-if (MyRank == Root)
-printf("\nEnter the number of intervals : ");
-scanf("% d",&NoInterval);
-/*....Broadcast the number of subintervals to each
-MPI_Bcast(&NoInterval, 1, MPI_INT, 0, MPI_COMM_WORLD);
-if (NoInterval <=0)
-{
-if (MyRank == Root)
-printf(printf("Invalid Value for Number of Intervals ..... \n")
+int rank, i;
+char Message[BUFFER_SIZE];
+MPI_Init (&argc, &argv);
+MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+strcpy(Message, "Hello World");
+MPI_Bcast ((void *)&Message, 1, MPI_INT, 0, MPI_COMM_WORLD);
+printf ("Rank :%d Message :%s\n", rank, Message);
+MPI_Barrier (MPI_COMM_WORLD);
 MPI_Finalize();
-exit(-1);
-}
-h = 1.0 / ( double )NoInterval;
-sum = 0.0;
-for (interval = MyRank + 1; interval <= NoInterval; interval += Numprocs)
-{
-x = h * (( double )interval-0.5);
-sum += func(x);
-}
-mypi = h * sum;
-/*....Collect the areas calculated in
-MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, Root, MPI_COMM_WORLD);
-if (MyRank == Root)
-{
-printf("pi is approximately %.16f, Error is %.16f \n",pi, fabs(pi-PI25DT));
-}
-MPI_Finalize();
+return 0;
 }
