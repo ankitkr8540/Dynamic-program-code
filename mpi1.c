a887d682e6155d5dd05b92c77cd0cdf5c42e5123
ankitkr8540 <33374891+ankitkr8540@users.noreply.github.com>
2019-12-04 09:27:06 +0530

    Update mpi1.c

--- mpi1.c
+++ mpi1.c
@@ -2,7 +2,7 @@
 #include<mpi.h>
 #include<string.h>
 #define BUFFER_SIZE 32
-int main( int argc, char *
+int main( int argc, char *argv[])
 {
 int MyRank,Numprocs, Destination, iproc;
 int tag = 0;
@@ -12,10 +12,8 @@ MPI_Init(&argc,&argv);
 MPI_Status status;
 MPI_Comm_rank(MPI_COMM _WORLD,&MyRank);
 MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
-/* print host name, and send message from process with rank 0 to all other processes
 if (MyRank == 0)
 {
-system("hostname");
 strcpy(Message, "Hello India");
 for (temp=1; temp<Numprocs;temp++)
 {
@@ -24,7 +22,6 @@ MPI_Send(Message, BUFFER_SIZE, MPI_CHAR, temp,tag,MPI_COMM_WORLD);
 }
 else
 {
-system("hostname");
 MPI_Recv(Message, BUFFER_SIZE, MPI_CHAR, Root,tag,MPI_COMM_WORLD, &status);
 }
 MPI_Finalize();
